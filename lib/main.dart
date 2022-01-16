6ab1f6ae42990d17c9b9c85143296cbd965bb620
Babar Hussain <hussainbabarkh@gmail.com>
2022-01-16 18:26:09 +0500

    day4 commited

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:my_app/pages/home_page.dart';
 import 'package:my_app/pages/login_page.dart';
 
@@ -12,18 +13,21 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      themeMode: ThemeMode.dark,
-      // darkTheme: ThemeData(brightness: Brightness.dark),
-      title: 'Flutter Demo A',
-      theme: ThemeData(
-        primarySwatch: Colors.deepPurple,
-      ),
-      initialRoute: '/Home',
-      home:const Login(title: "Login Page"),
-      routes: {
-        "/Home":(context)=>const Home(title: "My Home Page")
-      },
+        debugShowCheckedModeBanner: false,
+        themeMode: ThemeMode.dark,
+        // darkTheme: ThemeData(brightness: Brightness.dark),
+        title: 'Flutter Demo A',
+        theme: ThemeData(
+            primarySwatch: Colors.deepPurple,
+            fontFamily: GoogleFonts.lato().fontFamily
+    ),
+    initialRoute: '/',
+    // home:const Login(title: "Login Page"),
+    routes: {
+    "/": (context) => const Login(title: ""),
+    "/Home": (context) => const Home(title: "My Home Page"),
+    "/Login": (context) => const Login(title: "Login Page")
+    },
     );
   }
 }
--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class Login extends StatefulWidget {
   const Login({Key? key, required this.title}) : super(key: key);
@@ -12,14 +13,79 @@ class Login extends StatefulWidget {
 class _LoginState extends State<Login> {
   @override
   Widget build(BuildContext context) {
-    return  Scaffold(
-      appBar: AppBar(
-        centerTitle: true,
-        elevation: 0,
-        backgroundColor: Colors.orangeAccent,
-        title: Text(widget.title),
-      ),
-      body: Center(child: Text(widget.title, style: const TextStyle(fontSize: 30, color: Colors.blue)))
+    return Material(
+        color: Colors.white,
+        child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisSize: MainAxisSize.max,
+            children: [
+              const SizedBox(height: 20),
+              Image.asset("assets/images/login_image.png"),
+              const Text(
+                "Welcome to Crypto App",
+                style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(top: 30, left: 40, right: 40),
+                child: Column(
+                  children: [
+                    TextFormField(
+                      decoration: const InputDecoration(
+                          hintText: "Enter your email",
+                          labelText: "Email type"),
+                    ),
+                    const SizedBox(height: 20),
+                    TextFormField(
+                      obscureText: true,
+                      decoration: const InputDecoration(
+                          hintText: "Enter your password",
+                          labelText: "Password type"),
+                    ),
+                    const SizedBox(height: 20),
+                    ElevatedButton(
+                      style:TextButton.styleFrom(
+                        elevation: 5,
+                        textStyle: TextStyle(fontSize: 22, fontFamily: GoogleFonts.adamina().fontFamily),
+                        fixedSize:Size(150,50),
+                      ),
+                        onPressed: () {
+                          // showDialog(
+                          //   context: context,
+                          // git  builder: (BuildContext context) {
+                          //     return myDialog(
+                          //         "Login Successful", "Welcome, Are you sure want to enter in Alien space");
+                          //   },
+                          // );
+                        },
+                        child: const Text("Login"))
+                  ],
+                ),
+              )
+            ]));
+  }
+
+  myDialog(String title, String content) {
+    return AlertDialog(
+      title: Text(title, style: TextStyle(fontWeight: FontWeight.bold),),
+      content: Text(content),
+      actions: [
+        TextButton(
+          child: const Text("Continue",
+              style: TextStyle(fontWeight: FontWeight.bold)),
+          onPressed: () {
+            Navigator.of(context).pop(); // dismiss dialog
+          },
+        ),
+        TextButton(
+          child: const Text("Cancel",
+              style: TextStyle(fontWeight: FontWeight.bold, color: Colors.red)),
+          onPressed: () {
+            Navigator.of(context).pop();
+            // dismiss dialog
+          },
+        )
+      ],
     );
   }
 }
